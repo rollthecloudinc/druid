5150672f88fa6816dcf321a726fcb99aaafcd2d7
oddz <todd@sme-s-key.com>
2009-07-09 18:37:58 -0400

    changed

--- script/rake.php
+++ script/rake.php
@@ -1,13 +1,20 @@
 <?php
-if($argc != 6) { ?>
 
-	options: host,user,db,password and target are required
-	
-<?php }
+/*
+* This will eventually be a command line script to generate all model files
+*/
 
-require_once(str_replace('//','/',dirname(__FILE__).'/') .'../core/connection/connection.php');
 require_once(str_replace('//','/',dirname(__FILE__).'/') .'../rake/rake.php');
+require_once(str_replace('//','/',dirname(__FILE__).'/') .'../core/connection/connection.php');
 
-$db = new Connection($argv[1],$argv[2],$argv[3],$argv[4]);
+/* $args = array();
 
+if($argc!=1 && (($argc-1)%2)==0) {
+	$i=1;
+	while($i<$argc) {
+		$args[$argv[$i++]] = $argv[$i++];
+	}
+}*/
+
+$db = new Connection($argv[1],$argv[2],$argv[3],$argv[4]);*/
 ?>
